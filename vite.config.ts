4f8ee88490d6069b5bf7e233039ef05283a6eb40
korobasssss <an.korobeynikova@gmail.com>
2024-11-08 14:18:16 +0300

    preparing lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
+import dts from "vite-plugin-dts";
+import path from "path";
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/lib/index.ts"),
+      name: "ReactTSLib",
+      formats: ["es", "umd"],
+      fileName: (format) => `react-ts-lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "classnames"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "classnames": "cx",
+        },
+      },
+    },
+  },
 })
