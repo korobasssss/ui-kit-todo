324e17be3abd703cbca34bf0bd034b10d39a744e
korobasssss <an.korobeynikova@gmail.com>
2024-11-12 12:46:45 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import svgr from 'vite-plugin-svgr';
+//import cssInjectedByJs from 'vite-plugin-css-injected-by-js';
 
 // https://vite.dev/config/
 export default defineConfig({
